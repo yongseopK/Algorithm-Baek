02adecbec9b00d6577b85d9bdf6266dbe17091e4
Seop <114211344+yongseopK@users.noreply.github.com>
2023-12-18 16:03:48 +0900

    [level unrated] Title: 문자열 돌리기, Time: 221.47 ms, Memory: 69.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String[] list = a.split("");
+        
+        for(int i = 0; i < list.length; i++){
+            System.out.println(list[i]);
+        }
+    }
+}
