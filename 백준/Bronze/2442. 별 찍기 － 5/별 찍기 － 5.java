97a5cef6d7f40f0c6b92b528aebdc01d038aee20
Seop <114211344+yongseopK@users.noreply.github.com>
2024-03-06 11:23:12 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 412 ms, Memory: 19036 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        sc.close();
+
+        for(int i = 1; i <= N; i++) {    // 삼각형의 높이
+            for(int blank = 1; blank <= N - i; blank++){    // 앞 공백
+                System.out.print(" ");
+            }
+            for(int count = 0; count < 2 * i - 1; count++) {    // 삼각형의 길이
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
