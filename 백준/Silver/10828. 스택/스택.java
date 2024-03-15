0cca5ebc4b8516c9c5aa896965cdbf947551dcf9
Seop <114211344+yongseopK@users.noreply.github.com>
2024-03-15 16:09:43 +0900

    [Silver IV] Title: 스택, Time: 356 ms, Memory: 18724 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int[] stack;
+    static int size = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        stack = new int[N];
+
+
+        for(int i = 0; i < N; i++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    System.out.println(pop());
+                    break;
+                case "size":
+                    System.out.println(size());
+                    break;
+                case "empty":
+                    System.out.println(empty());
+                    break;
+                case "top":
+                    System.out.println(top());
+                    break;
+            }
+        }
+    }
+
+    public static void push(int item) {
+        stack[size] = item;
+        size++;
+    }
+
+    public static int pop() {
+        if(size == 0) {
+            return -1;
+        }
+        else {
+            int res = stack[size - 1];
+            stack[size - 1] = 0;
+            size--;
+            return res;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if(size == 0) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if(size == 0) {
+            return -1;
+        }
+        else {
+            return stack[size - 1];
+        }
+    }
+}
