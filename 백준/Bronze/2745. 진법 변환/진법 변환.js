fb167c32ed883979226fbf81720f97e42b4364f2
Seop <114211344+yongseopK@users.noreply.github.com>
2024-04-02 15:48:40 +0900

    [Bronze II] Title: 진법 변환, Time: 196 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.js
+++ 백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -2,6 +2,18 @@ const fs = require('fs');
 const inputs = fs.readFileSync('/dev/stdin').toString().split(' ');
 
 const N = inputs[0];
-const B = inputs[1];
+const B = parseInt(inputs[1]);
 
-console.log(parseInt(N, B));
+const getDecimalValue = (char) => {
+  if (char >= '0' && char <= '9') return char.charCodeAt(0) - 48;
+  return char.charCodeAt(0) - 55;
+};
+
+let decimalValue = 0;
+const len = N.length;
+
+for (let i = 0; i < len; i++) {
+  decimalValue += getDecimalValue(N[i]) * Math.pow(B, len - i - 1);
+}
+
+console.log(decimalValue);
